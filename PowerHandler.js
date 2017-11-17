a8106e374ae063635f2e88be2198f94a56b68d7b
johannkaiser <jonathandavis@clarke.k12.ga.us>
2017-11-17 11:27:51 -0500

    Create PowerHandler.js

---
+++ PowerHandler.js
@@ -0,0 +1,23 @@
+var mainBusAValue;
+var mainBusBValue;
+$(document).ready(function() {
+  switch(mainBusA) {
+    case 0:
+      mainBusAValue = false;
+    case 1:
+      mainBusAValue = true;
+  }
+  
+  switch(mainBusB) {
+    case 0:
+      mainBusBValue = false;
+    case 1:
+      mainBusBValue = true;
+  }
+  
+  if (mainBusAValue && mainBusBValue) {
+    alert("Power On");
+  } else {
+    alert("ErrorCode1");
+  }
+});
